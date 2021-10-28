1eec3b12f1a36654ba8861641eb19662534e5eb5
Wey Yeh <cweyyeh512@gmail.com>
2021-10-28 17:10:56 +0800

    Added WaveGANGeneratorV2 for signal to signal generation

--- GANs/wavegan.py
+++ GANs/wavegan.py
@@ -21,12 +21,13 @@ class Transpose1DBlock(nn.Module):
         padding=11,
         upsample=None, # Factor by which to upsample the feature map
         output_padding=1,
-        use_batch_norm=False, # Batch normalization for feature map mini batch
+        use_batch_norm=True, # Batch normalization for feature map mini batch
         device=device
     ): 
         super().__init__()
 
         self.upsample = upsample
+        self.padding = padding
         self.device = device
 
         operations = []
@@ -36,7 +37,7 @@ class Transpose1DBlock(nn.Module):
                 nn.Conv1d(
                     in_channels, out_channels,
                     kernel_size, stride,
-                    padding='same',
+                    padding=self.padding,
                     device=self.device
                 )
             )
@@ -55,6 +56,8 @@ class Transpose1DBlock(nn.Module):
             operations.append(
                 nn.BatchNorm1d(out_channels)
             )
+
+        operations.append(nn.ReLU())
         
         self.operations = nn.Sequential(*operations)
 
@@ -189,25 +192,30 @@ class WaveGANGenerator(nn.Module):
         # num_channels=[1024, 512, 256, 128, 64], # Number of channels of filters for each convolutional block
         num_channels=[1, 512, 256, 128, 64],
         stride=4,
+        padding=12,
         upsample=2, # Default 2
-        use_batch_norm=False,
+        use_batch_norm=True,
         device=device):
         super().__init__()
 
         # Ensure power of 2 for computation of dimensions
         assert math.ceil(np.log2(out_dim)) == math.floor(np.log2(out_dim))
+        if upsample:
+            assert upsample > 1
 
         self.in_dim = in_dim
         self.out_dim = out_dim
 
         self.use_batch_norm = use_batch_norm
         self.upsample = upsample
+        self.padding = padding
         self.stride = 1 if upsample else stride
         self.device = device
 
         # FC layer
+        scale = 4 if not self.upsample else self.upsample    
         self.fc_out_channels = num_channels[0]
-        self.fc_length = (self.out_dim // (self.upsample ** len(num_channels)))
+        self.fc_length = (self.out_dim // (scale ** len(num_channels)))
         fc_out_dim = self.fc_out_channels * self.fc_length
         fc_block = [
             nn.Linear(self.in_dim, fc_out_dim)
@@ -232,20 +240,20 @@ class WaveGANGenerator(nn.Module):
                     in_channels, out_channels,
                     kernel_size=kernel_size,
                     stride=self.stride,
+                    padding=self.padding,
                     upsample=self.upsample,
                     use_batch_norm=self.use_batch_norm,
                     device=self.device
                 )
             )
-            conv_blocks.append(
-                nn.ReLU()
-            )
+            
         # Final convolutional block to reduce to single channel waveform
         conv_blocks.append(
             Transpose1DBlock(
                 num_channels[-1], 1,
                 kernel_size=kernel_size,
                 stride=self.stride,
+                padding=self.padding,
                 upsample=self.upsample,
                 use_batch_norm=self.use_batch_norm,
                 device=self.device
@@ -329,3 +337,132 @@ class WaveGANDiscriminator(nn.Module):
     def forward(self, x):
         x = self.conv_blocks(x)
         return self.fc_block(x)
+
+class WaveGANGeneratorV2(nn.Module):
+    """
+    Modified version of WaveGANGenerator that includes a conovlutional network
+    In front that produces the latent vector z that is fed into WaveGANGenerator as input
+    Adopts a autoencoder like architecture that allows for a variety of tasks based on generation
+    of signals according a given signal(e.g. domain adaptation, denoising)
+    """
+    def __init__(
+        self,
+        in_dim,
+        latent_dim=128,
+        kernel_size=25,
+        encoder_channels=[1, 64, 128, 256, 512], # Includes the singular channel from input
+        decoder_channels=[1, 512, 256, 128, 64],
+        encoder_stride=4, 
+        decoder_stride=4,
+        encoder_padding=11,
+        decoder_padding=12,
+        alpha=0.2,
+        upsample=2,
+        shift_factor=0,
+        use_batch_norm=True,
+        device=device
+    ) -> None:
+        super().__init__()
+
+        # Ensure power of 2 for computation of dimensions
+        assert math.ceil(np.log2(in_dim)) == math.floor(np.log2(in_dim))
+        if upsample:
+            assert upsample > 1
+
+        self.in_dim = in_dim
+        self.latent_dim = latent_dim
+        self.use_batch_norm = use_batch_norm
+        self.encoder_channels = encoder_channels
+        self.decoder_channels = decoder_channels
+        self.encoder_stride = encoder_stride
+        self.decoder_stride = 1 if upsample else decoder_stride
+        self.encoder_padding = encoder_padding
+        self.decoder_padding = decoder_padding
+        self.alpha = alpha
+        self.upsample = upsample
+        self.shift_factor = shift_factor
+        self.device = device
+
+        # Encoder architecture
+        # Encoder convolutional block
+        encoder_conv_blocks = []
+        for in_channels, out_channels in zip(encoder_channels, encoder_channels[1:]):
+            encoder_conv_blocks.append(
+                Conv1DBlock(
+                    in_channels, out_channels,
+                    kernel_size=kernel_size, 
+                    alpha=self.alpha,
+                    shift_factor=shift_factor,
+                    stride=self.encoder_stride,
+                    padding=self.encoder_padding,
+                    use_batch_norm=self.use_batch_norm,
+                    device=self.device
+                )
+            )
+        encoder_conv_blocks.append(nn.Flatten())
+        self.encoder_conv_blocks = nn.Sequential(*encoder_conv_blocks)
+
+        # Encoder FC block
+        fc_in_dim = self.in_dim // (self.encoder_stride ** (len(encoder_channels) - 1))
+        fc_in_dim *= encoder_channels[-1]
+        encoder_fc_block = [
+            nn.Linear(fc_in_dim, self.latent_dim, device=self.device),
+            nn.ReLU()
+        ]
+        self.encoder_fc_block = nn.Sequential(*encoder_fc_block)
+
+        # Decoder architecture - Same as WaveGANGenerator
+        # Decoder FC block
+        scale = 4 if not self.upsample else self.upsample
+        self.decoder_fc_out_channels = decoder_channels[0]
+        self.decoder_fc_length = self.in_dim // (scale ** len(decoder_channels))
+        self.decoder_fc_out_dim = self.decoder_fc_out_channels * self.decoder_fc_length
+        fc_block = [
+            nn.Linear(self.latent_dim, self.decoder_fc_out_dim)
+        ]
+        if self.use_batch_norm:
+            fc_block.append(
+                nn.BatchNorm1d(self.decoder_fc_out_channels)
+            )
+        
+        fc_block.append(
+            nn.ReLU()
+        )
+        self.decoder_fc_block = nn.Sequential(*fc_block)
+
+        # Decoder convolutional blocks
+        decoder_conv_blocks = []
+        for in_channels, out_channels in zip(decoder_channels, decoder_channels[1:]):
+            decoder_conv_blocks.append(
+                Transpose1DBlock(
+                    in_channels, out_channels,
+                    kernel_size=kernel_size,
+                    stride=self.decoder_stride,
+                    padding=self.decoder_padding,
+                    upsample=self.upsample,
+                    use_batch_norm=self.use_batch_norm,
+                    device=self.device
+                )
+            )
+        decoder_conv_blocks.append(
+            Transpose1DBlock(
+                decoder_channels[-1], 1,
+                kernel_size=kernel_size,
+                stride=self.decoder_stride,
+                padding=self.decoder_padding,
+                upsample=self.upsample,
+                use_batch_norm=self.use_batch_norm,
+                device=self.device
+            )
+        )
+        decoder_conv_blocks.append(
+            nn.Tanh()
+        )
+        self.decoder_conv_blocks = nn.Sequential(*decoder_conv_blocks)
+
+    def forward(self, x):
+        x = self.encoder_conv_blocks(x)
+        x = self.encoder_fc_block(x)
+        x = torch.unsqueeze(x, 1) # Add channel dimension
+        x = self.decoder_fc_block(x)
+        return self.decoder_conv_blocks(x)
